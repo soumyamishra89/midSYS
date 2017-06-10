6680cf69e27d5b47409b950d12a8c8c46600cc26
Selvaraj, Sudarson <sudarson.selvaraj@gt-arc.com>
2017-06-10 22:43:17 +0200

    registration page

--- app/src/main/java/digitalhealth/de/midsys/RegistrationActivity.java
+++ app/src/main/java/digitalhealth/de/midsys/RegistrationActivity.java
@@ -1,172 +1,83 @@
 package digitalhealth.de.midsys;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import digitalhealth.de.midsys.fragment.Registration_1;
+import digitalhealth.de.midsys.fragment.Registration_2;
+import digitalhealth.de.midsys.fragment.Registration_3;
+
+
 public class RegistrationActivity extends AppCompatActivity {
-    String callerType = "Learner";
-
-    Button registerButton;
-
-    private final Integer LOCATION_REQUEST = 1;
 
     private final String TAG = RegistrationActivity.class.getName();
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
-    private TextView countryName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        countryName = (TextView) findViewById(R.id.country_name);
 
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
-        String caller = sharedPref.getString("callerType", "");
-        if(!caller.isEmpty()) {
-            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogrp);
-            for (int i = 0; i < radioGroup.getChildCount(); i++) {
-                if (radioGroup.getChildAt(i) instanceof RadioButton) {
-                    RadioButton radioButton = (RadioButton) radioGroup.getChildAt(i);
-                    if(radioButton.getText().toString().equals(caller)){
-                        radioButton.setChecked(true);
-                        break;
-                    }
-                }
-            }
-        }
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        if(!checkOrRequestUserPermission(Manifest.permission.ACCESS_COARSE_LOCATION) || !checkOrRequestUserPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
-        } else {
-            String country_name = null;
-            LocationManager lm = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-            Geocoder geocoder = new Geocoder(getApplicationContext());
-            for(String provider: lm.getAllProviders()) {
-                @SuppressWarnings ("ResourceType") Location location = lm.getLastKnownLocation(provider);
-                if(location!=null) {
-                    try {
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        if(addresses != null && addresses.size() > 0) {
-                            country_name = addresses.get(0).getCountryName();
-                            break;
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            countryName.setText(country_name);
-        }
-        registerButton = (Button) findViewById(R.id.register_button);
-        registerButton.setEnabled(false);
-        registerButton.setOnClickListener(registerClickListener);
-        String[] array = {"Arabic", "Dari", "English",  "Parsi",  "Somali", "Dutch", "Danish", "Finnish", "German", "Portuguese", "French", "Italian", "Polish", "Spanish"};
-        List<String> languages = Arrays.asList(array);
-        Collections.sort(languages, new Comparator<String>() {
-            @Override
-            public int compare(String lhs, String rhs){
-                return lhs.compareTo(rhs);
-            }
-        });
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    Button.OnClickListener registerClickListener = new Button.OnClickListener() {
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Registration_1(), "Personal Details");
+        adapter.addFragment(new Registration_2(), "Experience");
+        adapter.addFragment(new Registration_3(), "Personality");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
         @Override
-        public void onClick(View v){
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
         }
 
-    };
-
-    public void onRadioButtonClicked(View view){
-        RadioButton radioButton = (RadioButton) view;
-        Boolean checked = radioButton.isChecked();
-        switch (radioButton.getText().toString()) {
-            case "Learner":
-                if (checked) {
-                    callerType = "Learner";
-                    registerButton.setEnabled(true);
-                }
-                break;
-            case "Native":
-                if (checked) {
-                    callerType = "Native";
-                    registerButton.setEnabled(true);
-                }
-                break;
-        }
-    }
-
-    private boolean checkOrRequestUserPermission(String permission) {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, permission)
-                    == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                return false;
-            }
-
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
         }
 
-        return true;
-    }
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == LOCATION_REQUEST){
-            boolean permissionGranted = true;
-            for(int i=0; i<grantResults.length; i++) {
-                if(grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                    permissionGranted = false;
-                    break;
-                }
-            }
-
-            if(permissionGranted){
-                String country_name = null;
-                LocationManager lm = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-                Geocoder geocoder = new Geocoder(getApplicationContext());
-                for(String provider: lm.getAllProviders()) {
-                    @SuppressWarnings ("ResourceType") Location location = lm.getLastKnownLocation(provider);
-                    if(location!=null) {
-                        try {
-                            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            if(addresses != null && addresses.size() > 0) {
-                                country_name = addresses.get(0).getCountryName();
-                                break;
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-                countryName.setText(country_name);
-            }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
 }
---
+++ app/src/main/java/digitalhealth/de/midsys/fragment/Registration_3.java
@@ -0,0 +1,30 @@
+package digitalhealth.de.midsys.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import digitalhealth.de.midsys.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Registration_3 extends Fragment {
+
+
+    public Registration_3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration_3, container, false);
+    }
+
+}

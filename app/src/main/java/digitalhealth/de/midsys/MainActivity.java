0ea206b1dfa0f02c2383970b85c2f8d7eb99e723
Soumya Siladitya Mishra <soumya.s.mishra@campus.tu-berlin.de>
2017-06-10 22:44:08 +0200

    updated code

--- app/src/main/java/digitalhealth/de/midsys/MainActivity.java
+++ app/src/main/java/digitalhealth/de/midsys/MainActivity.java
@@ -1,18 +1,31 @@
 package digitalhealth.de.midsys;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+import digitalhealth.de.midsys.fragment.FragmentHealth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final String TAG = MainActivity.class.getName();
 
@@ -23,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
 
     private Context thisContext;
+
+    ConstraintLayout layoutLocation, layoutStep, layoutArrow, layoutMain, layoutDropdown;
+    ImageView imgvArrow;
+    FrameLayout layoutFragmentContainer;
+    ConstraintSet constraintSet;
+    int lastClickSectionId = -1;
+
+    // This hashmap helps recolor the dropdown arrow to match the clicked section
+    HashMap<Integer, Integer> colorMap = new HashMap<>();
+
+    // This hashmap will map each section layout with the left most section below it
+    // So when the drop down fragment appear we can rearrange the layout constraints
+    HashMap<Integer, Integer> viewMap = new HashMap<>();
+
+    // This hashmap maps between section ID and corresponding fragment class
+    HashMap<Integer, String> fragmentMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -35,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         navigationView = (NavigationView) findViewById(R.id.snet_app_drawerView);
         navigationView.setNavigationItemSelectedListener(new NavigationDrawerItemClickListener());
+        navigationView.setBackgroundTintList(null);
         // ActionBarDrawerToggle ties together the proper interactions
         // between the sliding drawer and the action bar app icon
         actionBarDrawerToggle = new ActionBarDrawerToggle(
@@ -60,9 +90,58 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
+
+        //Populate colorMap
+        colorMap.put(R.id.layoutLocations, R.color.transparentRed);
+        colorMap.put(R.id.layoutSteps, R.color.transparentOrange);
+        colorMap.put(R.id.layoutMusic, R.color.transparentYellow);
+        colorMap.put(R.id.layoutAppUsage, R.color.transparentCyan);
+        colorMap.put(R.id.layoutInternetData, R.color.transparentPurple);
+
+        // Populate viewMap
+        viewMap.put(R.id.layoutLocations, R.id.layoutMusic);
+        viewMap.put(R.id.layoutSteps, R.id.layoutMusic);
+        viewMap.put(R.id.layoutMusic, R.id.layoutInternetData);
+        viewMap.put(R.id.layoutAppUsage, R.id.layoutInternetData);
+
+        //Populate fragmentMap
+        // #TODO implementer of each section fragment need to add an entry to this map
+        fragmentMap.put(R.id.layoutLocations, FragmentHealth.class.getName());
+        fragmentMap.put(R.id.layoutSteps, FragmentHealth.class.getName());
+        fragmentMap.put(R.id.layoutAppUsage, FragmentHealth.class.getName());
+        fragmentMap.put(R.id.layoutInternetData, FragmentHealth.class.getName());
+        fragmentMap.put(R.id.layoutMusic, FragmentHealth.class.getName());
+
+        layoutLocation = (ConstraintLayout) findViewById(R.id.layoutLocations);
+
+        layoutStep = (ConstraintLayout) findViewById(R.id.layoutSteps);
+
+        layoutDropdown = (ConstraintLayout) findViewById(R.id.layoutFragmentMain);
+        layoutFragmentContainer = (FrameLayout) findViewById(R.id.layoutFrameFragment);
+        imgvArrow = (ImageView) findViewById(R.id.imgvDownArrow);
+        layoutArrow = (ConstraintLayout) findViewById(R.id.layoutArrow);
+
+        layoutMain = (ConstraintLayout) findViewById(R.id.layoutMain);
+
+        constraintSet = new ConstraintSet();
+        constraintSet.clone(layoutMain);
 
     }
 
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState){
+        super.onPostCreate(savedInstanceState, persistentState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.syncState();
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
     /**
      * Click listener for the Navigation View menu items
      */
@@ -101,4 +180,102 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
+
+
+    @Override
+    public void onClick(View v) {
+        // If the clicked object is a section, do rearrangement
+        int currentClickedViewId = v.getId();
+
+        if (colorMap.containsKey(currentClickedViewId)) {
+            // Click on same section again
+            if (currentClickedViewId == lastClickSectionId) {
+                if (isDropDownFragmentVisible())
+                    hideDropDownFragment();
+                else {
+                    loadNewFragment(currentClickedViewId);
+                }
+            } else {  // User choose new section
+                if (isDropDownFragmentVisible()) {
+                    hideDropDownFragment();
+                    loadNewFragment(currentClickedViewId);
+                } else
+                    loadNewFragment(currentClickedViewId);
+            }
+        } else
+            return;
+    }
+
+    private void loadNewFragment(int clickedSectionId) {
+
+        // Avoid exception when click on section with unimplemented fragment
+        if (!fragmentMap.containsKey(clickedSectionId))
+            return;
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //Hide current display fragment
+        if(fragmentManager.findFragmentByTag(fragmentMap.get(lastClickSectionId))!= null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.hide(
+                    fragmentManager.findFragmentByTag(fragmentMap.get(lastClickSectionId)));
+            fragmentTransaction.commit();
+        }
+
+        // Load corresponding fragment into framelayout
+        String tag = fragmentMap.get(clickedSectionId);
+        if(fragmentManager.findFragmentByTag(fragmentMap.get(clickedSectionId))==null) {
+            // Each fragment get chosen date in milliseconds from this bundle
+            //Bundle fragmentBundle = new Bundle();
+            //fragmentBundle.putLong(StaticReferences.BUNDLE_CHOSEN_DATE_KEY, chosenTimestamp);
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            Fragment fragment = Fragment.instantiate(this, tag);
+            fragmentTransaction.add(R.id.layoutFrameFragment, fragment, tag);
+            fragmentTransaction.commit();
+        }
+        else {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.show(fragmentManager.findFragmentByTag(tag));
+            fragmentTransaction.commit();
+        }
+
+//        Fragment fragment;
+//        fragment = Fragment.instantiate(this, fragmentMap.get(clickedSectionId));
+//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//        transaction.replace(R.id.layoutFrameFragment, fragment);
+//        transaction.commit();
+
+        reArrangeConstraints(clickedSectionId);
+
+        imgvArrow.setColorFilter(getResources().getColor(colorMap.get(clickedSectionId)));
+        layoutArrow.setVisibility(View.VISIBLE);
+        layoutDropdown.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDropDownFragment() {
+        layoutArrow.setVisibility(View.GONE);
+        layoutDropdown.setVisibility(View.GONE);
+    }
+    private boolean isDropDownFragmentVisible() {
+        return (layoutArrow.getVisibility() == View.VISIBLE);
+    }
+
+    private void reArrangeConstraints(int clickedSectionId) {
+        // If the user clicks on different section, need to restore the old constraints first
+        if (clickedSectionId != lastClickSectionId && lastClickSectionId != -1 && viewMap.containsKey(lastClickSectionId)) {
+            constraintSet.connect(viewMap.get(lastClickSectionId),
+                    ConstraintSet.TOP, lastClickSectionId, ConstraintSet.BOTTOM);
+        }
+        lastClickSectionId = clickedSectionId;
+
+        constraintSet.connect(layoutArrow.getId(), ConstraintSet.TOP, clickedSectionId, ConstraintSet.BOTTOM);
+        constraintSet.connect(layoutArrow.getId(), ConstraintSet.LEFT, clickedSectionId, ConstraintSet.LEFT);
+        constraintSet.connect(layoutArrow.getId(), ConstraintSet.RIGHT, clickedSectionId, ConstraintSet.RIGHT);
+        // Last row sections don't need this constraints
+        if (viewMap.containsKey(clickedSectionId))
+            constraintSet.connect(viewMap.get(clickedSectionId),
+                    ConstraintSet.TOP, layoutDropdown.getId(), ConstraintSet.BOTTOM);
+        constraintSet.applyTo(layoutMain);
+    }
+
 }
---
+++ app/src/main/java/digitalhealth/de/midsys/fragment/FragmentHealth.java
@@ -0,0 +1,25 @@
+package digitalhealth.de.midsys.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import digitalhealth.de.midsys.R;
+
+/**
+ * Created by Soumya Mishra on 10/06/17.
+ */
+
+public class FragmentHealth extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View root = inflater.inflate(R.layout.fragment_health, container, false);
+
+        return root;
+    }
+}
